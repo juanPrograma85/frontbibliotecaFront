19707c09f821beefcdf77c28270e7219af9ee7e9
JUAN LEAL <ec0683j@comcel.com.co>
2023-01-10 14:41:13 -0500

    Cambio de apuntamiento

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_sergeneral: 'https://172.30.160.211:8080'
+  url_sergeneral: 'https://biblioteca-services-leal-nevardosa-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com'
 };
